6f267bfc2e2db9b64b6979a9110c036222843c7e
unknown <sw00384@SHUWEI.COM>
2020-06-03 15:03:35 +0800

    调整IP池

--- crawl.py
+++ crawl.py
@@ -115,17 +115,19 @@ class dp_meishi:
                     self.insert_comment(comm_kwargs_list)
             else:
                 print('code 不是200：--------',json_resp['code'])
+                get_success(self.proxy)
+                self.proxy = get_ip()
                 if self.page == 1:
                     redis_cli.sadd(redis_name, self.args)
                 # 如果翻页到50以后，不能将IP认定为不能使用
-                if self.page < 50:
-                    # get_error(self.proxy)
-                    get_success(self.proxy)
-                    self.proxy = get_ip()
-                elif 50 < self.page < 100:
-                    get_success(self.proxy)
-                    self.proxy = get_ip()
-                else:
+                # if self.page < 50:
+                #     # get_error(self.proxy)
+                #     get_success(self.proxy)
+                #     self.proxy = get_ip()
+                # elif 50 < self.page < 100:
+                #     get_success(self.proxy)
+                #     self.proxy = get_ip()
+                if self.page > 100:
                     self.page = self.page_count
 
         else:
