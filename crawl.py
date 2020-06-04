3a292a20a9d1220c7fc903b035876079d1ed4fa9
unknown <sw00384@SHUWEI.COM>
2020-06-04 16:09:15 +0800

    调整

--- crawl.py
+++ crawl.py
@@ -32,9 +32,9 @@ def get_hc_v():
 
 class dp_meishi:
     def __init__(self,dp_args):
-        self.city_name = '武汉市'
-        self.city_en_name = 'wuhan'
-        self.cityId = '16'
+        self.city_name = '沈阳市'
+        self.city_en_name = 'shenyang'
+        self.cityId = '18'
         self.proxy = get_ip()
         self.g_id = None
         self.r_id = None
