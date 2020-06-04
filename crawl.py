870374326c9682517e243b5fb03c6f085ddb6ce6
unknown <sw00384@SHUWEI.COM>
2020-06-04 15:46:57 +0800

    调整

--- crawl.py
+++ crawl.py
@@ -32,9 +32,9 @@ def get_hc_v():
 
 class dp_meishi:
     def __init__(self,dp_args):
-        self.city_name = '宁波市'
-        self.city_en_name = 'ningbo'
-        self.cityId = '11'
+        self.city_name = '长沙市'
+        self.city_en_name = 'changsha'
+        self.cityId = '344'
         self.proxy = get_ip()
         self.g_id = None
         self.r_id = None
--- tools/wirte_category.py
+++ tools/wirte_category.py
@@ -11,8 +11,8 @@ from lxml import etree
 # conn = psycopg2.connect(database="crawler", user="root", password="9TTjkHY^Y#UeLORZ", host="10.101.0.90", port="8635")
 # cur = conn.cursor()
 proxy = {'http':'http://122.192.174.187:43781','https':'https://122.192.174.187:43781'}
-city_en = 'ningbo'
-city_zh = '宁波'
+city_en = 'changsha'
+city_zh = '长沙'
 headers = {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'Accept-Encoding': 'gzip, deflate',
