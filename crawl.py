c8638ee05b710983ba8c7f8b287e9c44b5e98e1e
unknown <sw00384@SHUWEI.COM>
2020-06-08 18:32:15 +0800

    调整

--- crawl.py
+++ crawl.py
@@ -32,9 +32,9 @@ def get_hc_v():
 
 class dp_meishi:
     def __init__(self,dp_args):
-        self.city_name = '南京市'
-        self.city_en_name = 'nanjing'
-        self.cityId = '5'
+        self.city_name = '苏州市'
+        self.city_en_name = 'suzhou'
+        self.cityId = '6'
         self.chtype = '20'
         self.chtype_name = '购物'
         self.proxy = get_ip()
