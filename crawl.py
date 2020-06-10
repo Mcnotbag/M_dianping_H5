ee910c80b4e7d0447930d669d1718beda06d46aa
unknown <sw00384@SHUWEI.COM>
2020-06-10 19:53:49 +0800

    休闲娱乐

--- crawl.py
+++ crawl.py
@@ -32,9 +32,9 @@ def get_hc_v():
 
 class dp_meishi:
     def __init__(self,dp_args):
-        self.city_name = '武汉市'
-        self.city_en_name = 'wuhan'
-        self.cityId = '16'
+        self.city_name = '重庆市'
+        self.city_en_name = 'chongqing'
+        self.cityId = '9'
         self.chtype = '30'
         self.chtype_name = '休闲娱乐'
         self.proxy = get_ip()
