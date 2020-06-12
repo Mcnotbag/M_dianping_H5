565a31780a3801a8e3cda017ea72a29c5ff33665
unknown <sw00384@SHUWEI.COM>
2020-06-12 12:50:38 +0800

    学习培训

--- crawl.py
+++ crawl.py
@@ -32,9 +32,9 @@ def get_hc_v():
 
 class dp_meishi:
     def __init__(self,dp_args):
-        self.city_name = '苏州市'
-        self.city_en_name = 'suzhou'
-        self.cityId = '6'
+        self.city_name = '成都市'
+        self.city_en_name = '成都'
+        self.cityId = '8'
         self.chtype = '75'
         self.chtype_name = '学习培训'
         self.proxy = get_ip()
